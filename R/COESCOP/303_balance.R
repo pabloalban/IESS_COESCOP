f213b015172a3caf191218771681f22b7af0e0dd
pabloalban <pablo.alban@iess.gob.ec>
2023-07-10 09:25:10 -0500

    Impacto por entidad en los aportes

--- R/COESCOP/303_balance.R
+++ R/COESCOP/303_balance.R
@@ -51,6 +51,42 @@ impacto <- aportes %>%
           total_vap = cumsum( v * total_cor ) )
 
 
+## 2.1. Impacto por entidad-------------------------------------------------------------------------
+
+aux <- malla_ivm %>%
+  filter( anio >= 2023 & anio <= 2042 ) %>%
+  group_by( anio, tipo ) %>%
+  mutate( aporte_ivm = sum( aporte_ivm, na.rm = TRUE ),
+          aporte_salud = sum( aporte_salud, na.rm = TRUE ),
+          aporte_coescop = sum(aporte_coescop, na.rm = TRUE ) ) %>%
+  ungroup( ) %>%
+  distinct(., anio, tipo,  .keep_all = TRUE ) %>%
+  dplyr::select( anio, 
+                 tipo,
+                 aporte_ivm,
+                 aporte_salud,
+                 aporte_coescop ) %>%
+  dplyr::select( -aporte_coescop ) %>%
+  mutate( total_cor = aporte_ivm + aporte_salud ) %>%
+  group_by( tipo ) %>%
+  mutate( r = 1 + i_a ) %>%
+  mutate( r = if_else( anio == '2023',
+                       1, 
+                       r ) ) %>%
+  mutate( r = cumprod( r ) ) %>%
+  mutate( v = 1 / r ) %>%
+  mutate( aporte_ivm_vap = cumsum( v * aporte_ivm ),
+          aporte_salud_vap = cumsum( v * aporte_salud ),
+          total_vap = cumsum( v * total_cor ) ) %>%
+  ungroup( )
+
+impacto_entidades <- aux %>%
+  filter( anio == 2042)
+
+sum(impacto_entidades$total_vap)
+
+sum(impacto_entidades$aporte_ivm)
+
 #1. Balance-----------------------------------------------------------------------------------------
 reserva_mat_anual <- reserva_matematica %>%
   group_by( anio ) %>%
