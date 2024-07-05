c266123514c829df2b609b19317eed2dbe213144
pabloalban <pablo.alban@iess.gob.ec>
2024-07-05 11:07:43 -0500

    Concordia

--- R/COESCOP/600_reporte_latex.R
+++ R/COESCOP/600_reporte_latex.R
@@ -14,6 +14,7 @@ source( 'R/402_graf_contexto_economico.R', encoding = 'UTF-8', echo = FALSE )
 
 # Gráficos específicas de COESCOP ------------------------------------------------------------------
 source( 'R/COESCOP/400_graf_analisis_demografico.R', encoding = 'UTF-8', echo = FALSE )
+source( 'R/COESCOP/401_graf_analisis_demografico_concordia.R', encoding = 'UTF-8', echo = FALSE )
 
 # Tablas genéricas ---------------------------------------------------------------------------------
 source( 'R/501_tab_contexto_economico.R', encoding = 'UTF-8', echo = FALSE )
@@ -21,7 +22,7 @@ source( 'R/501_tab_contexto_economico.R', encoding = 'UTF-8', echo = FALSE )
 # Tablas específicas de COESCOP --------------------------------------------------------------------
 source( 'R/COESCOP/500_tab_analisis_demografico.R', encoding = 'UTF-8', echo = FALSE )
 source( 'R/COESCOP/501_tab_resultados.R', encoding = 'UTF-8', echo = FALSE )
-
+source( 'R/COESCOP/502_tab_analisis_demografico_concordia.R', encoding = 'UTF-8', echo = FALSE )
 
 # Reporte LaTeX ------------------------------------------------------------------------------------
 source( parametros$reporte_script, encoding = 'UTF-8', echo = FALSE )
